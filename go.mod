6c3b91538d52f66155cc3da00a71279873570a05
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-10-20 10:28:19 +0300

    impllementation

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/PaulShpilsher/genrsa
 
 go 1.21.2
+
+require golang.org/x/crypto v0.14.0
+
+require golang.org/x/sys v0.13.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,91 @@
 package main
 
-func main() {
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"log"
+	"os"
 
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	bits, pemFile, pubFile := initConfig()
+	log.Printf("generating RSA key (bit size %d) -%s, %s", bits, pemFile, pubFile)
+
+	privateKey := createPrivateKey(bits)
+
+	pemBinary := getPemBinary(privateKey)
+
+	pubBinary := getPublicKeyBinary(&privateKey.PublicKey)
+
+	writeFile(pemFile, pemBinary)
+	log.Printf("private key saved to \"%s\" file", pemFile)
+
+	writeFile(pubFile, pubBinary)
+	log.Printf("public key saved to \"%s\" file", pubFile)
+
+}
+
+// initConfig reads options from the command line arguments
+func initConfig() (bits int, pemFile string, pubFile string) {
+	var output string
+
+	flag.IntVar(&bits, "bits", 4096, "Key size (eg. 4096)")
+	flag.StringVar(&output, "output", "rsa", "Output file name base. (eg. specifying \"foo\" writes files \"foo.prv\", \"foo.pem\", and \"foo.pub\")")
+	flag.Parse()
+
+	pemFile = output + ".pem"
+	pubFile = output + ".pub"
+	return bits, pemFile, pubFile
+}
+
+// createPrivateKey generates a random RSA private key of the given bit size.
+func createPrivateKey(bits int) *rsa.PrivateKey {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		log.Fatalf("private key generation failed. err: %v\n", err)
+	}
+
+	if err = privateKey.Validate(); err != nil {
+		log.Fatalf("private key validation failed. err: %v\n", err)
+	}
+
+	log.Println("private key created")
+	return privateKey
+}
+
+// getPublicKeyBinary take a rsa.PublicKey and return bytes suitable for writing to .pub file
+// returns in the format "ssh-rsa ..."
+func getPublicKeyBinary(publicKey *rsa.PublicKey) []byte {
+	publicRsaKey, err := ssh.NewPublicKey(publicKey)
+	if err != nil {
+		log.Fatalf("public key creation failed. err: %v\n", err)
+	}
+
+	pubKeyBytes := ssh.MarshalAuthorizedKey(publicRsaKey)
+
+	log.Println("public key created")
+	return pubKeyBytes
+}
+
+// getPemBinary encodes Private Key from RSA to PEM format
+func getPemBinary(privateKey *rsa.PrivateKey) []byte {
+	pemBinary := pem.EncodeToMemory(&pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   x509.MarshalPKCS1PrivateKey(privateKey), // Get ASN.1 DER format
+	})
+
+	log.Println("PEM created")
+	return pemBinary
+}
+
+func writeFile(fileName string, data []byte) {
+	if err := os.WriteFile(fileName, data, 0600); err != nil {
+		log.Fatalf("writing \"%s\" file failed. err: %v\n", fileName, err)
+	}
 }
